30d2d3848e8c73fa09094d4833f395d1d459be0d
jcunneen <jcunneen@cs.stonybrook.edu>
2018-02-27 15:58:54 -0500

    dao interface updated with implemented methods

--- src/main/java/com/revature/dao/ChatroomDao.java
+++ src/main/java/com/revature/dao/ChatroomDao.java
@@ -4,4 +4,6 @@ public interface ChatroomDao {
 
 	public String getNameById(int id);
 	public int getIdByName(String name);
+	public void deleteChatroom(int id);
+	public void addChatroom(String name);
 }

31daa74fa736c9f3ec209116734dd561ca6db0dd
jcunneen <jcunneen@cs.stonybrook.edu>
2018-02-27 14:19:49 -0500

    chatroom dao and implementation actually done

--- src/main/java/com/revature/dao/ChatroomDaoImpl.java
+++ src/main/java/com/revature/dao/ChatroomDaoImpl.java
@@ -1,11 +1,16 @@
 package com.revature.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import com.revature.beans.Chatroom;
+import com.revature.domain.Bat;
 import com.revature.util.HibernateUtil;
 
-public class ChatroomDaoImpl implements ChatroomDao{
+public class ChatroomDaoImpl implements ChatroomDao {
 
 	public String getNameById(int id) {
 		Session s = HibernateUtil.getSession();
@@ -19,6 +24,31 @@ public class ChatroomDaoImpl implements ChatroomDao{
 		Chatroom c = (Chatroom) s.get(Chatroom.class, name);
 		s.close();
 		return c.getId();
+	}
+
+	public void deleteChatroom(int id) {
+		Session s = HibernateUtil.getSession();
+		Transaction tx = s.beginTransaction();
+		try {
+			String hql = "DELETE FROM CHATROOM WHERE ID = :id";
+			Query query = s.createQuery(hql);
+			query.setInteger("id", id);
+			query.executeUpdate();
+
+			tx.commit();
+		} catch (Exception t) {
+			tx.rollback();
+		}
+	}
+
+	public void addChatroom(String name) {
+		Chatroom c = new Chatroom(name);
+		Session s = HibernateUtil.getSession();
+		Transaction tx = s.beginTransaction();
+			s.persist(c);
+		tx.commit();
+		s.close();
+
 	}
 
 }

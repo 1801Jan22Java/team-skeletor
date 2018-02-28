6c588707fcb4a6a4a1e8bc9f3393b0f82fee9422
jaboyles <jacobboyles0115@gmail.com>
2018-02-28 09:22:22 -0500

    message dao added

---
+++ src/main/java/com/revature/dao/MessageDao.java
@@ -0,0 +1,18 @@
+package com.revature.dao;
+
+import java.util.List;
+
+import com.revature.beans.Message;
+
+public interface MessageDao {
+	public void createMessage(Message msg);
+
+	public Message getMessageById(int msgId);
+
+	public List<Message> getMessages();
+
+	public void deleteMessage(int msgId);
+
+	public void updateMessage(Message msg);
+
+}

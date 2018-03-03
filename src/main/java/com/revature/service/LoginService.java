c3fdec5083107c983601d47d5a2f1d11f87c60d2
Omar Mohamad <omarbmohamad@gmail.com>
2018-03-03 13:08:03 -0500

    LoginService, LoginController created, UserDao modified, UserService, UserController modified

---
+++ src/main/java/com/revature/service/LoginService.java
@@ -0,0 +1,24 @@
+package com.revature.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.beans.User;
+import com.revature.dao.UserDaoImpl;
+
+@Service(value="loginService")
+
+	
+public class LoginService {
+	@Autowired
+	UserDaoImpl userDaoImpl;
+	
+	public User getUserByCredentials(String username, String password){
+		return userDaoImpl.getUserByCredentials(username,password);
+	}
+	
+	public User getUserByUsername(String username) {
+		return userDaoImpl.getUserByUsername(username);
+	}
+
+}

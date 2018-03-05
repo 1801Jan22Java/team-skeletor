60a863047bc5fa53a193b6b837bacee2d0a8200d
Omar Mohamad <omarbmohamad@gmail.com>
2018-03-05 14:46:09 -0500

    CrossOrigin annotation added to Controllers

--- src/main/java/com/revature/controller/UserController.java
+++ src/main/java/com/revature/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import com.revature.service.UserService;
 
 @Controller("userController")
 @RequestMapping("/user")
+@CrossOrigin (origins="http://localhost:8084")
 public class UserController {
 
 	@Autowired
